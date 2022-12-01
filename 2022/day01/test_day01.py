8e867ecd7d5e2fa7c8379defedd88d2a98257a27
Chris Leavoy <cleavoy@etsy.com>
2022-12-01 09:44:59 -0500

    Add 2022 w/ day01

---
+++ 2022/day01/test_day01.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+
+def read(filename: str):
+    filename = Path(__file__).parent / filename
+    with open(filename, encoding='utf-8') as f:
+        calories, elves = 0, []
+        for line in f.readlines():
+            l = line.strip()
+            if l == "":
+                elves.append(calories)
+                calories = 0
+            else:
+                calories += int(l)
+        elves.append(calories)
+        return elves
+
+
+def test_sol1():
+    elves = read('day01-input.txt')
+    assert max(elves) == 69795
+
+
+def test_sol2():
+    elves = read('day01-input.txt')
+    elves.sort()
+    sum_top3 = sum(elves[-3:])
+    assert sum_top3 == 208437
---
+++ start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+[[ $1 -ge 1 && $1 -le 25 ]] || (echo "\$1 must be a day [01-25]"; exit 1)
+
+year=2022
+day="day$1"
+day_short=$(echo $1 | sed 's/^0*//')
+base=$(dirname $0)/$year/$day
+session=$(cat $(dirname $0)/.session)
+
+[[ -d $base ]] || mkdir -p $base
+cd $base
+
+touch "$day-input.txt"
+
+cat > "test_$day.py" <<EOF
+from pathlib import Path
+
+def read(filename: str) -> str:
+    filename = Path(__file__).parent / filename
+    with open(filename, encoding='utf-8') as f:
+        return f.read()
+
+def test_sol1():
+    s = read('$day-input.txt')
+    assert s
+
+# def test_sol2():
+EOF
+
+curl -s "https://adventofcode.com/$year/day/$day_short/input" \
+    -H "Cookie: session=$session" \
+    -o "$day-input.txt"
+
+echo "See https://adventofcode.com/$year/day/$day_short"
