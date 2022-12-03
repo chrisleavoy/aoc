ccb64e927638c3e4e802c00500501bed9a11924f
Chris Leavoy <chris@sdk.ca>
2022-12-03 12:23:04 -0500

    lint start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-[[ $1 -ge 1 && $1 -le 25 ]] || (echo "\$1 must be a day [01-25]"; exit 1)
+[[ $1 -ge 1 && $1 -le 25 ]] || (echo "\$1 must be a day [1-25]"; exit 1)
 
 year=2022
-day="day$1"
-day_short=$(echo $1 | sed 's/^0*//')
-base=$(dirname $0)/$year/$day
-session=$(cat $(dirname $0)/.session)
+day=$(printf "day%02d" "$1")
+day_short=$(printf "%d" "$1")
+base=$(dirname "$0")/$year/$day
+session=$(cat "$(dirname "$0")/.session")
 
-[[ -d $base ]] || mkdir -p $base
-cd $base
+[[ -d $base ]] || mkdir -p "$base"
+cd "$base"
 
 touch "$day-input.txt"
 
